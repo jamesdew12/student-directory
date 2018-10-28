73070330add1978968cce0c8b2a039b570b7afe1
James de Winton <james.dewinton@gmail.com>
2018-10-28 01:12:36 +0100

    near completion

--- directory.rb
+++ directory.rb
@@ -5,13 +5,34 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  puts "What makers cohort are you in?"
+  cohort = gets.chomp.to_sym
+  puts "What are his/her hobbie?"
+  hobbie = gets.chomp
+  puts "what country you from"
+  country = gets.chomp
+  if cohort.to_s == ""
+    cohort = "unknown cohort"
+  end  
+
+
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, hobbie: hobbie, country: country}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
     name = gets.chomp
+    puts "What makers cohort are you in?"
+    cohort = gets.chomp.to_sym
+    if cohort.to_s == ""
+    cohort = "unknown cohort"
+    end
+    puts "What are his/her hobbie?"
+    hobbie = gets.chomp
+    puts "what country you from"
+    country = gets.chomp
   end
   # return the array of students
   students
@@ -23,10 +44,14 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  # iterate through students until studnet[:name] is the empty string
+  # print the right schnitzel
+  index = 0
+  until students[index] == nil
+    puts "#{index += 1}. #{students[index-1][:name]} (#{students[index-1][:cohort]} cohort) "
   end
 end
+
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
@@ -37,3 +62,7 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
+
+# #{students[index-1][:name]}
+# (#{students[index-1][:cohort]} cohort)
